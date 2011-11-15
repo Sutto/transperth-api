d1d2458be9249d62fadcc698dd7a3d6344467c61
Darcy Laycock <sutto@sutto.net>
2011-11-15 20:07:14 +0800

    Add basics of a blank app

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.1'
+gem 'pg'
+
+gem 'nokogiri'
+gem 'api_smith'
+gem 'rocket_pants', :git => 'git@github.com:jammbox/Rocketpants.git'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.1.4'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transperth::Application.config.secret_token = '7518e4860ffb02a5d18a582038026bc8f63d4679e277beff6adfef91831bbcbb12fa52761b5e76c986bb7670cad94dce8b4c9ba61f18d9f019c624a9145e3e95'
