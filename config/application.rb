e0a9dc843196e38a1818d73660145eaa4ccf38ae
Darcy Laycock <sutto@sutto.net>
2011-11-15 20:10:07 +0800

    Ignore sprockets

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,8 @@ require "active_resource/railtie"
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, Rails.env)
 end
 
 module Transperth
