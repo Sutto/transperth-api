2b062174324c207398336fd947e08e3a2173fc08
Darcy Laycock <sutto@sutto.net>
2012-01-08 22:14:41 +0800

    Enable caching again...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Transperth::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   # Disable the etag cache... temporarily atleast.
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
