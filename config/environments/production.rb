294f17a8ca2fa39b9a71d030744b57f9beb03af0
Darcy Laycock <sutto@sutto.net>
2012-01-08 22:17:06 +0800

    Not ready for the prime time...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Transperth::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   # Disable the etag cache... temporarily atleast.
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
